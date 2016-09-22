3ad92331158a9b0fab9f70d4d6f94e58f9dfd54b
David Reese <dave@freshconsulting.com>
2016-09-21 22:48:55 -0700

    adding intro and some responsive style tweaks

--- wp-content/themes/DR6/front-page.php
+++ wp-content/themes/DR6/front-page.php
@@ -2,7 +2,17 @@
 /**
  * Template Name: Front Page
  */
+?>
 
+<div class="row">
+  <div class="col-sm-12">
+    <div class="intro">
+      Hi, I'm Dave Reese, a web developer from the great PNW. I'm interested in creating - from web apps, to drawings, to beer - making things is what I am all about.
+    </div>
+  </div>
+</div>
+
+<?php
 $args = array(
   'post_type' => 'post',
   'post-status' => 'publish',
@@ -15,48 +25,50 @@ $used_colors2 = array();
 
 $work_query = new WP_Query( $args );
 if ( $work_query->have_posts() ) { ?>
-  <div class="col-md-10 col-md-offset-1">
-    <div class="grid">
-      <div class="grid-sizer"></div>
-  	  <div class="gutter-sizer"></div>
-      <?php while ( $work_query->have_posts() ) { $work_query->the_post();
-        $title = $work_query->post->post_title;
-        $letter = substr($title, 0, 1);
-        $new_title = substr($title, 1);
-        $date = explode('-', explode(' ',$work_query->post->post_date)[0]);
-        $link = get_field('link');
-        $github = get_field('github');
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1">
+      <div class="grid">
+        <div class="grid-sizer"></div>
+    	  <div class="gutter-sizer"></div>
+        <?php while ( $work_query->have_posts() ) { $work_query->the_post();
+          $title = $work_query->post->post_title;
+          $letter = substr($title, 0, 1);
+          $new_title = substr($title, 1);
+          $date = explode('-', explode(' ',$work_query->post->post_date)[0]);
+          $link = get_field('link');
+          $github = get_field('github');
 
-        if ( $link ) {
-          $link = '<a class="fa fa-globe" href="'.$link.'" target="_blank"></a>';
-        }
+          if ( $link ) {
+            $link = '<a class="fa fa-globe" href="'.$link.'" target="_blank"></a>';
+          }
 
-        if ( $github ) {
-          $github = '<a class="fa fa-github" href="'.$github.'" target="_blank"></a>';
-        }
+          if ( $github ) {
+            $github = '<a class="fa fa-github" href="'.$github.'" target="_blank"></a>';
+          }
 
-        if ( 1 <= count($colors)) {
-          $color = $colors[array_rand($colors, 1)];
-          $colors = array_diff($colors, array($color));
-          array_push($used_colors, $color);
-        } else {
-          $color = $used_colors[array_rand($used_colors, 1)];
-          $used_colors = array_diff($used_colors, array($color));
-          array_push($used_colors2, $used_colors);
-        }
+          if ( 1 <= count($colors)) {
+            $color = $colors[array_rand($colors, 1)];
+            $colors = array_diff($colors, array($color));
+            array_push($used_colors, $color);
+          } else {
+            $color = $used_colors[array_rand($used_colors, 1)];
+            $used_colors = array_diff($used_colors, array($color));
+            array_push($used_colors2, $used_colors);
+          }
 
-        echo '<div class="grid-item grid-item--closed block-container">';
-          echo '<div class="block-container__block '.$color.'">';
-            echo '<div class="block-container__block-content">';
-              echo '<div class="block-container__title"><span>'.$letter.'</span><span>'.$new_title.'</span></div>';
-              echo '<div class="block-container__copy">'.$work_query->post->post_content.'</div>';
-              echo '<div class="block-container__meta">'.$link.''.$github.'</div>';
-              echo '<div class="block-container__date">'.$date[1].'-'.$date[0].'</div>';
-            echo '</div>';
-            echo '<div class="block-container__border"></div>';
-            echo '</div>';
-        echo '</div>';
-      } ?>
+          echo '<div class="grid-item grid-item--closed block-container">';
+            echo '<div class="block-container__block '.$color.'">';
+              echo '<div class="block-container__block-content">';
+                echo '<div class="block-container__title"><span>'.$letter.'</span><span>'.$new_title.'</span></div>';
+                echo '<div class="block-container__copy">'.$work_query->post->post_content.'</div>';
+                echo '<div class="block-container__meta">'.$link.''.$github.'</div>';
+                echo '<div class="block-container__date">'.$date[1].'-'.$date[0].'</div>';
+              echo '</div>';
+              echo '<div class="block-container__border"></div>';
+              echo '</div>';
+          echo '</div>';
+        } ?>
+      </div>
     </div>
   </div>
   <?php wp_reset_postdata();
--- wp-content/themes/DR6/templates/head.php
+++ wp-content/themes/DR6/templates/head.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico">
   <?php wp_head(); ?>
 </head>
